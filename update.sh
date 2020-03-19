43d1da60115f70b2bb4f444d967a0aedaad9fc5d
Sen <46561474+obsoletenerd@users.noreply.github.com>
2020-03-19 12:29:49 +1100

    Bash script to update CSVs

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv -O confirmed.csv
+
+wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv -O deaths.csv
+
+wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv -O recovered.csv
